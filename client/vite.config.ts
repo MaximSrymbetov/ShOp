dafa8b03939392e95db708b0a40a9c1bef04be0d
admin <admin@MacBook-Pro.local>
2024-01-18 18:34:07 +0300

    config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000/',
+        changeOrigin: true,
+      },
+      '/img': {
+        target: 'http://localhost:4000/',
+        changeOrigin: true,
+      },
+    },
+  },
 });
