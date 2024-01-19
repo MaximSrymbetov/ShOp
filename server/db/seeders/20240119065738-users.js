96c341d2d957436d01290a41ba23b1cbc736f7a5
HenryShuller <yuryshuller1996@gmail.com>
2024-01-19 10:51:47 +0300

    BD4.0

---
+++ server/db/seeders/20240119065738-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+      await queryInterface.bulkInsert('Users', [
+        {
+        name: 'ADMIN',
+        phone:'88005553555',
+        email: 'I_am_ADMIN@mail.ru',
+        password: await bcrypt.hash('123456789', 10),
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Klient',
+        phone:'8909',
+        email: 'I_am_NOT_ADMIN@mail.ru',
+        password: await bcrypt.hash('123456789', 10),
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+
+      await queryInterface.bulkDelete('People', null, {});
+     
+  }
+};
