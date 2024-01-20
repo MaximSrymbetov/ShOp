081ed5d09bd22ab94d1b071f2c9bb5125aa72286
admin <admin@MacBook-Pro.local>
2024-01-20 16:15:43 +0300

    registration and authorization

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './App/App';
 import './index.css';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </BrowserRouter>,
+  <Provider store = {store}>
+    <BrowserRouter>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </BrowserRouter>
+  </Provider>,
 );
